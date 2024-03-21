30e15038a4c1dd4b41259d9999ba7a541aae9bcd
RadityaIH <radityadave10@gmail.com>
2024-03-21 14:54:59 +0700

    harusnya fix masalah favicon be

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="shortcut icon" href="../public/favicon/favicon.ico" />
       </Head>
       <body>
         <Main />
