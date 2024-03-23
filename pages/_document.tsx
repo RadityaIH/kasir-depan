ee6b0af058e5c0427531f04a4155df2c03d7428e
RadityaIH <radityadave10@gmail.com>
2024-03-23 12:55:39 +0700

    favicon lg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="#" />
+        <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
       <body>
         <Main />
